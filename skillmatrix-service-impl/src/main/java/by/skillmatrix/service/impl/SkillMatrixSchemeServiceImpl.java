71117987ce3049caf0801e74e4f44e080ca1464d
Vadim Denisik <denisik.vadim@yandex.by>
2022-05-05 20:20:33 +0300

    Add NotFoundException and ExceptionHandler for it

--- skillmatrix-service-impl/src/main/java/by/skillmatrix/service/impl/SkillMatrixSchemeServiceImpl.java
+++ skillmatrix-service-impl/src/main/java/by/skillmatrix/service/impl/SkillMatrixSchemeServiceImpl.java
@@ -16,9 +16,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.swing.text.html.parser.Entity;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 @Slf4j
