5723438dcecefde171f0d50bc34dcdad933f544a
Vadim Denisik <denisik.vadim@yandex.by>
2022-05-05 17:30:13 +0300

    entity imports

--- skillmatrix-entity/src/main/java/by/skillmatrix/entity/SkillCategoryEntity.java
+++ skillmatrix-entity/src/main/java/by/skillmatrix/entity/SkillCategoryEntity.java
@@ -4,7 +4,17 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.OneToMany;
+import javax.persistence.FetchType;
+import javax.persistence.CascadeType;
 import java.util.List;
 
 @Data
